30e3baace541393f51bf4b0caa9b10a8a79f2528
brenmiller27 <167664968+brenmiller27@users.noreply.github.com>
2025-06-12 21:21:23 -0700

    intitial commit

--- home/scripts/app.js
+++ home/scripts/app.js
@@ -1,1 +1,18 @@
 //create hamburger menu
+
+const navEL = document.querySelector('.nav');
+
+const hamburger = document.querySelector('.hamburger')
+
+hamburgerEL.addeventlistner("click", () => {
+
+    navEL.classList.toggle('nav--open');
+    
+    hamburgerEL.classList.toggle('.hamburger--open');
+})
+
+navEL.addeventlistner('click', () => {
+    navEL.classList.remove('nav--open');
+    hamburger.classList.remove('hamburger--open');
+
+})
